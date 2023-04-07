8beff65f199807bb6be753636b759acaeb24052d
Eshaan Debnath <eshaandebnath@gmail.com>
2023-04-07 07:08:55 -0400

    Fixed merging stuff?

--- build/scripts/home.js
+++ build/scripts/home.js
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
-// const learnMoreButton = document.querySelector('#homeHeader button');
-// learnMoreButton.addEventListener('click', () => {
-//     window.location.href = '#homeContent';
-// })
-=======
 const learnMoreButton = document.querySelector('#homeHeader button');
 learnMoreButton.addEventListener('click', () => {
     window.location.href = '#homeContent';
 })
->>>>>>> 586ad8aa751587bb724ac7ac8fa91f25341110df
--- src/scripts/home.js
+++ src/scripts/home.js
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
-const learnMoreButton = document.querySelector('#homeHeader button');
-learnMoreButton.addEventListener('click', () => {
-    window.location.href = '#homeContent';
-})
-=======
 // const learnMoreButton = document.querySelector('#homeHeader button');
 // learnMoreButton.addEventListener('click', () => {
 //     window.location.href = '#homeContent';
 // })
->>>>>>> 22978665426b11a01571f8af054c007255c26cce
