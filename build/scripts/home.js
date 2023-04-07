e4b7d478cdad5803b732d38d98603a89c3d282d8
Eshaan Debnath <eshaandebnath@gmail.com>
2023-04-07 07:09:55 -0400

    More stuff ig?

--- build/scripts/home.js
+++ build/scripts/home.js
@@ -1,4 +1,4 @@
-const learnMoreButton = document.querySelector('#homeHeader button');
-learnMoreButton.addEventListener('click', () => {
-    window.location.href = '#homeContent';
-})
+// const learnMoreButton = document.querySelector('#homeHeader button');
+// learnMoreButton.addEventListener('click', () => {
+//     window.location.href = '#homeContent';
+// })
