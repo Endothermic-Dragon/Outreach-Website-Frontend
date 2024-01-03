0a59d0fe215a8ad1acc0b1a399412918717ad069
Eshaan Debnath <69125881+Endothermic-Dragon@users.noreply.github.com>
2024-01-02 21:09:16 -0500

    MASSIVE speedup for initiatives page Also disabled images for initiatives

--- src/scripts/initiatives.js
+++ src/scripts/initiatives.js
@@ -7,26 +7,45 @@ fetch(`${backend}./initiatives`, {
 	},
 }).then(async (data) => {
 	data = await data.json();
+	let leads = Array.from(new Set(data.flatMap((el) => el.leads)));
+	let leadsObj = {};
+
+	await fetch(`${backend}./translate-user-ids`, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({
+			user_ids: leads,
+		}),
+	}).then(async (data) => {
+		const mapped = await data.json();
+		for (let i = 0; i < leads.length; i++) {
+			leadsObj[leads[i]] = mapped[i];
+		}
+	});
+
 	data = data.map(async (el) => {
 		el.mins = parseInt(el.total_mins) % 60;
 		el.hours = Math.floor(parseInt(el.total_mins) / 60);
+		el.leads = el.leads.map((el) => leadsObj[el]);
 
 		// Sanitize
 		let div = document.createElement("div");
 		div.innerText = el.description;
 		el.description = div.innerHTML.replaceAll("<br>", "</div><div>");
 
-		await fetch(`${backend}./translate-user-ids`, {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify({
-				user_ids: el.leads,
-			}),
-		}).then(async (data) => {
-			el.leads = await data.json();
-		});
+		// await fetch(`${backend}./translate-user-ids`, {
+		// 	method: "POST",
+		// 	headers: {
+		// 		"Content-Type": "application/json",
+		// 	},
+		// 	body: JSON.stringify({
+		// 		user_ids: el.leads,
+		// 	}),
+		// }).then(async (data) => {
+		// 	el.leads = await data.json();
+		// });
 
 		// First name, last initial
 		// el.leads = el.leads.map(el => {
